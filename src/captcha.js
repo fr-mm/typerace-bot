3c8660df008ca3da1a77d6b2b2e091f17a8cfdf5
chico <chico.zimcolor@gmail.com>
2022-06-13 18:41:25 -0300

    fixed being stuck in captcha waiting due to missing await

--- src/captcha.js
+++ src/captcha.js
@@ -1,13 +1,17 @@
 const CAPTCHA_PAGE_TITLE = "Attention Required! | Cloudflare";
 
 module.exports.waitForManualCompletion = async (page) => {
-  while (pageIsCaptcha(page)) {
+  console.log("Waiting for manual captcha solving");
+  while (await pageIsCaptcha(page)) {
     await page.waitForTimeout(1000);
   }
   console.log("Captcha solved");
-  await page.waitForNavigation();
 };
 
 const pageIsCaptcha = async (page) => {
-  return (await page.title()) === CAPTCHA_PAGE_TITLE;
+  try {
+    return (await page.title()) === CAPTCHA_PAGE_TITLE;
+  } catch (error) {
+    return true;
+  }
 };
